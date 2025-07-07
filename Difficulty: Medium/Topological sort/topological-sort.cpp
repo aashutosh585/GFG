e2627486a77799bc26b8370c7dcd2bd9337df8a0
Ashutosh Maurya <aashutoshmaurya585@gmail.com>
2025-07-07 16:27:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -11,6 +11,7 @@ class Solution {
         
         st.push(node);
     }
+    
     vector<int> topoSort(int V, vector<vector<int>>& edges) {
         // code here
         vector<vector<int>>adj(V);
@@ -18,21 +19,47 @@ class Solution {
             adj[it[0]].push_back(it[1]);
         }
         
-        vector<bool>visited(V,0);
+        // vector<bool>visited(V,0);
         
-        stack<int>st;
-        for(int i=0;i<V;i++){
-            if(!visited[i]){
-                dfs(i,st,adj,visited);
+        // stack<int>st;
+        // for(int i=0;i<V;i++){
+        //     if(!visited[i]){
+        //         dfs(i,st,adj,visited);
+        //     }
+        // }
+        
+        vector<int>ans;
+        // while(!st.empty()){
+        //     ans.push_back(st.top());
+        //     st.pop();
+        // }
+        
+        
+        // find in degree each node
+        vector<int>inDeg(V,0);
+        for(int i=0;i<V;i++)
+        for(int j=0;j<adj[i].size();j++)
+        inDeg[adj[i][j]]++;
+        
+        // indegree whose 0
+        queue<int>q;
+        for(int i=0;i<V;i++)
+        if(!inDeg[i])
+        q.push(i);
+        
+        // remove edge from that indegree
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(int i=0;i<adj[node].size();i++){
+                
+                inDeg[adj[node][i]]--;
+                if(inDeg[adj[node][i]]==0) q.push(adj[node][i]);
             }
         }
         
-        vector<int>ans;
-        while(!st.empty()){
-            // cout<<""<<st.top()<<""<<endl;
-            ans.push_back(st.top());
-            st.pop();
-        }
         
         return ans;
         
