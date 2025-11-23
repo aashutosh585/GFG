ca7a0c876ff67d73b6b29ab8ccc4d14bbe3d2b52
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-11-23 20:01:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -1,66 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    
-    int maxLen(int i,int n,vector<int>&arr,vector<int>&dp){
-        if(dp[i]!=-1) return dp[i];
-        
-        int cnt=1;
-        for(int j=i+1;j<n;j++){
-            if(arr[i]<arr[j]){
-               cnt=max(cnt,1+maxLen(j,n,arr,dp));
-            }
-        }
-        dp[i]=cnt;
-        return dp[i];
-      
-    }
-    
     int lis(vector<int>& arr) {
         // code here
-        int n=arr.size(),ans=0;
-        vector<int>dp(n,-1);
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         
-        for(int i=0;i<n;i++){
-            ans= max(ans,maxLen(i,n,arr,dp));
+        for(int i=n-1;i>=0;i--){
+            for(int pi=i-1;pi>=-1;pi--){
+                int tak=0;
+                if(pi==-1 || arr[i]>arr[pi]) tak=1+dp[i+1][i+1];
+                int ntak=dp[i+1][pi+1];
+                dp[i][pi+1]=max(tak,ntak);
+            }
         }
-       
-        return ans;
+        return dp[0][0];
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the integer input
-
-    while (t--) {
-        int n;
-        vector<int> arr;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            arr.push_back(num);
-
-        Solution obj;
-        cout << obj.lis(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
